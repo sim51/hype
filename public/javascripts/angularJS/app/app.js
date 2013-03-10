b27dcb0166f740c10bd2551055bca9de86a5dcb2
Benoît Simard <bsimard@logisima.com>
2013-03-10 21:26:06 +0100

    i18n

--- public/javascripts/angularJS/app/app.js
+++ public/javascripts/angularJS/app/app.js
@@ -6,8 +6,15 @@ var app = angular.module('hope', ['github', 'play']);
 /* Configure the application with the route */
 app.config(function($routeProvider) {
 		$routeProvider
+            .when('/', {templateUrl:'/assets/javascripts/angularJS/partials/home.html', controller: HomeCtrl})
+            .when('/about', {templateUrl:'/assets/javascripts/angularJS/partials/about.html', controller: AboutCtrl})
+            .when('/contact', {templateUrl:'/assets/javascripts/angularJS/partials/contact.html', controller: ContactCtrl})
+            .when('/user', {templateUrl:'/assets/javascripts/angularJS/partials/user.html', controller: UserCtrl})
+            .when('/user/:user', {templateUrl:'/assets/javascripts/angularJS/partials/user.html', controller: UserCtrl})
+            .when('/prez/:id', {templateUrl:'/assets/javascripts/angularJS/partials/user.html', controller: PrezViewCtrl})
+            .when('/prez/edit/:id', {templateUrl:'/assets/javascripts/angularJS/partials/user.html', controller: PrezEditCtrl})
 			.when('/error', {templateUrl: '/assets/javascripts/angularJS/partials/error.html', controller: ErrorCtrl})
-	      	.otherwise({redirectTo: '/error'});
+	      	.otherwise({redirectTo: '/'});
 	});
 
 /* What we do when application start ? */
