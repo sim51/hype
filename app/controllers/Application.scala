173342b80254a5d9a2947d61f2698fc6d0767bce
Benoît Simard <bsimard@logisima.com>
2013-03-10 17:37:34 +0100

    application header

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,23 +6,31 @@ import securesocial.core.UserId
 import play.api.mvc.Cookie
 import scala.Some
 
+/**
+ * Application's controllers.
+ * @author bsimard
+ */
 object Application extends Controller with securesocial.core.SecureSocial {
-  
+
+  /**
+   * Default action to get angularJS application.
+   * @return
+   */
   def index = UserAwareAction { implicit request =>
     request.user match {
       case Some(user) => {
         user.oAuth2Info match {
           case Some(oAuthInfo2) => {
-            Ok(views.html.user(user, request.acceptLanguages.map(_.code).mkString(", "), "Coucou"))
+            Ok(views.html.index(request.user, request.acceptLanguages.map(_.code).mkString(", "), "Coucou"))
               .withCookies(Cookie("token",oAuthInfo2.accessToken))
           }
           case None => {
-            Ok(views.html.guest(request.acceptLanguages.map(_.code).mkString(", "), "Coucou"))
+            Ok(views.html.index(request.user, request.acceptLanguages.map(_.code).mkString(", "), "Coucou"))
           }
         }
       }
       case None => {
-        Ok(views.html.guest(request.acceptLanguages.map(_.code).mkString(", "), "Coucou"))
+        Ok(views.html.index(request.user, request.acceptLanguages.map(_.code).mkString(", "), "Coucou"))
       }
     }
   }
