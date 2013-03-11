78ca90d8b2e35ffdf6344a59655201f5b06884e1
Benoît Simard <bsimard@logisima.com>
2013-03-11 23:28:17 +0100

    enhance form contact with succes alert

--- public/javascripts/angularJS/app/controllers.js
+++ public/javascripts/angularJS/app/controllers.js
@@ -15,16 +15,17 @@ function HomeCtrl() {}
  */
 function ContactCtrl($scope, Play) {
     $scope.sendMail = function(){
-         Play.sendMail($scope.name, $scope.email, $scope.message);
+        // No future response, but if there is an error, we are redirect
+        // to error page by the service !
+        Play.sendMail($scope.name, $scope.email, $scope.message);
+        $scope.success=true
     }
 }
 
 /**
  *	Contact controller : do you want to contact me ?
  */
-function ContactCtrl($scope, Play) {
-    $scope.sendMail = function(){
-         Play.sendMail($scope.name, $scope.email, $scope.message);
-    }
+function ProfileCtrl($scope, Play) {
+
 }
 
