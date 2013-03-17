3fd15162c5f6b20dfe1c8db91b3d2e9d97ec7326
Benoît Simard <bsimard@logisima.com>
2013-03-17 23:04:11 +0100

    webscoket OK

--- public/template/revealjs/ws-see.js
+++ public/template/revealjs/ws-see.js
@@ -3,7 +3,8 @@ var socket = new WS("ws://localhost:9000/prez/ws?id=###ID###");
 
 var receiveEvent = function(event) {
     console.log("[WS]: receiving " + event.data)
-    Reveal.slide(event.h,event.v);
+    var data = JSON.parse(event.data);
+    Reveal.slide(data.h,data.v);
 }
 socket.onmessage = receiveEvent;
 
