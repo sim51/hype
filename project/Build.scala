6b951b09bc4dabb49d36ecfbb94368247baff92e
Benoît Simard <bsimard@logisima.com>
2013-03-17 21:09:27 +0100

    play 2.1

--- project/Build.scala
+++ project/Build.scala
@@ -8,12 +8,14 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "securesocial" %% "securesocial" % "2.0.12",
-      "com.typesafe" %% "play-plugins-mailer" % "2.0.4"
+      "securesocial" %% "securesocial" % "master-SNAPSHOT",
+      "com.typesafe" %% "play-plugins-mailer" % "2.1.0"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      resolvers += Resolver.url("sbt-plugin-snapshots", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
+      resolvers += Resolver.url("sbt-plugin-snapshots", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns),
+      resolvers += Resolver.url("sbt-plugin-release", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns),
+      resolvers += Resolver.url("typesafe-release", url("http://repo.typesafe.com/typesafe/releases/"))(Resolver.ivyStylePatterns)
     )
 
 }
