f00d2acfb3302cfdd2fc36216a0ba43c5b64a576
Benoît Simard <bsimard@logisima.com>
2013-10-19 13:46:26 +0200

    upgrade to play 2.2.0

--- project/Build.scala
+++ project/Build.scala
@@ -9,13 +9,14 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "securesocial" %% "securesocial" % "master-SNAPSHOT",
+      "securesocial" %% "securesocial" % "logisima-SNAPSHOT",
       "com.typesafe" %% "play-plugins-mailer" % "2.1.0",
       jdbc,
       anorm
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
+      resolvers += "Local Play Repository" at "file://usr/local/play/play2/current/repository/local",
       resolvers += Resolver.url("sbt-plugin-snapshots", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns),
       resolvers += Resolver.url("sbt-plugin-release", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns),
       resolvers += Resolver.url("typesafe-release", url("http://repo.typesafe.com/typesafe/releases/"))(Resolver.ivyStylePatterns)
